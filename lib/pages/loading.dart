bec8fe0ab9f31f7f21e07fe0aacab09fd3289416
guccibase <tahirnasri96@gmail.com>
2020-01-09 01:14:37 -0500

    fix

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,7 +1,5 @@
-import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
 import 'package:world_time/services/world_time.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
